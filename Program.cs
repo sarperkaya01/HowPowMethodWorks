87abc12c33220fd3133d7c0c7bb183b1dcc74891
sarperkaya01 <sarperkaya01@gmail.com>
2021-11-05 23:39:45 +0300

    firstCommit

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace HowPowMethodWorks
+{
+    class Program
+    {
+        static int Pow2(int a,int b){
+            if(b==0){
+                return 1;
+            }
+            else {
+                return  a*Pow2(a,b-1);
+            }
+        }
+        static void Main(string[] args)
+        {
+            int a =int.Parse(Console.ReadLine());
+            int b =int.Parse(Console.ReadLine());
+            
+            Console.WriteLine(Pow2(a,b));
+            Console.ReadLine();
+        }
+    }
+}
